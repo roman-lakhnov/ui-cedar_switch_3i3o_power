c139cdeb320fb2644009ac7c82b3387a0c731471
Roman Lakhnov <lakhnov.roman@gmail.com>
2026-05-03 12:56:19 +0300

    update build conf

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
+import viteCompression from 'vite-plugin-compression'
 
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), viteCompression()],
 	server: {
 		host: '0.0.0.0', // чтобы Vite был доступен по локальной сети
 		port: 5173,
@@ -18,6 +19,15 @@ export default defineConfig({
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, './src')
+		}
+	},
+	build: {
+		rollupOptions: {
+			output: {
+				entryFileNames: 'assets/[name].js',
+				chunkFileNames: 'assets/[name].js',
+				assetFileNames: 'assets/[name].[ext]',
+			}
 		}
 	}
 })
